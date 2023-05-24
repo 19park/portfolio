0a4718c4fe6b8f4a6405ff01494df0cf6082c623
parkjunghwan <19park@recokr.com>
2023-05-24 16:56:25 +0900

    update scene

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,108 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import {onMounted} from "vue";
+import {init} from "./scene.js";
+
+onMounted(init);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="loading">
+    <svg class="loading-circle">
+      <circle cx="50%" cy="50%" r="25"></circle>
+    </svg>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="container">
+    <section class="scroll-section" id="scroll-section-0">
+      <h1>test..</h1>
+
+      <div class="sticky-elem sticky-elem-canvas">
+        <canvas id="video-canvas-0" width="1920" height="1080"></canvas>
+      </div>
+      <div class="sticky-elem main-message a">
+        <p>text</p>
+      </div>
+    </section>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style scoped lang="scss">
+@import "assets/scss/common";
+
+.container {
+  overflow-x: hidden;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.scroll-section {
+  position: relative;
+  padding-top: 50vh;
+
+  /**
+    기본적으로 보이지않고, body의 id에 따라서 스크롤이 어떤섹션에 위치했을때 보일지 정한다.
+   */
+  .sticky-elem {
+    display: none;
+    position: fixed;
+    left: 0;
+    width: 100%;
+
+    &.sticky-elem-canvas {
+      top: 0;
+      height: 100%;
+
+      canvas {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+      }
+    }
+  }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#show-scene-0 #scroll-section-0 .sticky-elem {
+  display: block;
+  will-change: transform, opacity;
+}
+
+.loading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 100;
+  background: white;
+  opacity: 0;
+  transition: 0.5s;
+}
+.before-load .container {
+  display: none;
+}
+.before-load .loading {
+  opacity: 1;
+}
+@keyframes loading-spin {
+  100% { transform: rotate(360deg); }
+}
+@keyframes loading-circle-ani {
+  0% { stroke-dashoffset: 157; }
+  75% { stroke-dashoffset: -147; }
+  100% { stroke-dashoffset: -157; }
+}
+.loading-circle {
+  width: 54px;
+  height: 54px;
+  animation: loading-spin 3s infinite;
+}
+.loading-circle circle {
+  stroke: black;
+  stroke-width: 4;
+  /* getTotalLength()로 stroke의 길이를 얻어올 수 있음 */
+  stroke-dasharray: 157, 157;
+  stroke-dashoffset: 0;
+  fill: transparent;
+  animation: loading-circle-ani 1s infinite;
+  /* transition: 1s; */
 }
 </style>
